5ef504c0956f095abd62c40987d411c45736f33f
Ferran Collado <https://github.com/FerranCollado/Prova-SOC.git>
2025-12-04 12:45:47 +0100

    Primer Commit desde casa

---
+++ Exe.02/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exe._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edat,edatfutura;
+            
+            Console.WriteLine("Quina edat tens? ");
+            edat = Convert.ToInt32(Console.ReadLine());
+
+            edatfutura = edat + 1;
+
+            Console.WriteLine("L'any que ve tindràs " + edatfutura);
+
+
+
+
+                
+
+            
+
+
+
+
+
+
+        }
+    }
+}
